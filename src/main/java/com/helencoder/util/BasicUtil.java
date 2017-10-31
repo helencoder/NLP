46596ec5e9fb957cc3b88dfe0657e2d04e829784
helencoder <731873664@qq.com>
2017-10-31 21:07:50 +0800

    公共方法类库

---
+++ src/main/java/com/helencoder/util/BasicUtil.java
@@ -0,0 +1,22 @@
+package com.helencoder.util;
+
+import java.util.Random;
+
+/**
+ * 公公方法类
+ *
+ * Created by zhenghailun on 2017/10/31.
+ */
+public class BasicUtil {
+
+    public static String verifyCode(int length) {
+        String dict = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder verifyCode = new StringBuilder(length);
+        Random random = new Random();
+        for(int i = 0; i < length; i++){
+            verifyCode.append(dict.charAt(random.nextInt(dict.length())));
+        }
+        return verifyCode.toString();
+    }
+
+}
