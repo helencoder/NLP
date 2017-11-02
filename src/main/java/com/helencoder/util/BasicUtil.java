f913a9920c3b0ab8dff2e9651850477f34eab9ce
helencoder <731873664@qq.com>
2017-11-02 11:14:39 +0800

    添加字符串匹配查找方法

--- src/main/java/com/helencoder/util/BasicUtil.java
+++ src/main/java/com/helencoder/util/BasicUtil.java
@@ -1,5 +1,6 @@
 package com.helencoder.util;
 
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -9,6 +10,12 @@ import java.util.Random;
  */
 public class BasicUtil {
 
+    /**
+     * 生成验证码
+     *
+     * @param length 验证码长度
+     * @return String 验证码
+     */
     public static String verifyCode(int length) {
         String dict = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         StringBuilder verifyCode = new StringBuilder(length);
@@ -17,6 +24,40 @@ public class BasicUtil {
             verifyCode.append(dict.charAt(random.nextInt(dict.length())));
         }
         return verifyCode.toString();
+    }
+
+    /**
+     * 字符串查找
+     *
+     * @param pattern 待查找的pattern
+     * @param str 查找的字符串
+     * @return boolean
+     */
+    public static boolean isPatternExists(String pattern, String str) {
+        int index = KMP.search(str, pattern);
+        return index != -1;
+    }
+
+    /**
+     * 获取最长公共子序列
+     *
+     * @param str1 字符串1
+     * @param str2 字符串2
+     * @return String 最长公共子序列
+     */
+    public static String getCommonSequence(String str1, String str2) {
+        return LCS.commonSequence(str1, str2);
+    }
+
+    /**
+     * 获取最长公共子串
+     *
+     * @param str1 字符串1
+     * @param str2 字符串2
+     * @return String 最长公共子串
+     */
+    public static List<String> getCommonString(String str1, String str2) {
+        return LCS.commonString(str1, str2);
     }
 
 }
