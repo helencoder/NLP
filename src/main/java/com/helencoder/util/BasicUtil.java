d2706261a31283b8f4a30a49fb553f50b95bf3fc
helencoder <731873664@qq.com>
2017-11-22 19:51:39 +0800

    添加公共方法

--- src/main/java/com/helencoder/util/BasicUtil.java
+++ src/main/java/com/helencoder/util/BasicUtil.java
@@ -1,10 +1,12 @@
 package com.helencoder.util;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 公共方法类
- *
+ * <p>
  * Created by zhenghailun on 2017/10/31.
  */
 public class BasicUtil {
@@ -19,7 +21,7 @@ public class BasicUtil {
         String dict = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         StringBuilder verifyCode = new StringBuilder(length);
         Random random = new Random();
-        for(int i = 0; i < length; i++){
+        for (int i = 0; i < length; i++) {
             verifyCode.append(dict.charAt(random.nextInt(dict.length())));
         }
         return verifyCode.toString();
@@ -29,7 +31,7 @@ public class BasicUtil {
      * 字符串查找
      *
      * @param pattern 待查找的pattern
-     * @param str 查找的字符串
+     * @param str     查找的字符串
      * @return boolean
      */
     public static boolean isPatternExists(String pattern, String str) {
@@ -61,6 +63,7 @@ public class BasicUtil {
 
     /**
      * 数组转换为字符串
+     *
      * @param list
      * @param delimiter 分隔符
      * @return 字符串
@@ -82,7 +85,8 @@ public class BasicUtil {
 
     /**
      * Map排序(按键值排序)
-     * @param map (Map)
+     *
+     * @param map  (Map)
      * @param flag true降序 false升序
      * @return sortedMap
      */
@@ -106,5 +110,70 @@ public class BasicUtil {
         return result;
     }
 
+    /**
+     * 判断字符串中是否包含数字
+     *
+     * @param str 字符串
+     * @return boolean
+     */
+    public static Boolean isNumbersInclude(String str) {
+        Pattern p = Pattern.compile(".*\\d+.*");
+        Matcher m = p.matcher(str);
+        return m.matches();
+    }
+
+    /**
+     * 判断字符串中是否包含字母
+     *
+     * @param str 字符串
+     * @return boolean
+     */
+    public static Boolean isCharacterInclude(String str) {
+        String regex = ".*[a-zA-Z]+.*";
+        Matcher m = Pattern.compile(regex).matcher(str);
+        return m.matches();
+    }
+
+    /**
+     * 半角转全角
+     *
+     * @param input String.
+     * @return 全角字符串.
+     */
+    public static String ToSBC(String input) {
+        char c[] = input.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] == ' ') {
+                c[i] = '\u3000';
+            } else if (c[i] < '\177') {
+                c[i] = (char) (c[i] + 65248);
+
+            }
+        }
+        return new String(c);
+    }
+
+    /**
+     * 全角转半角
+     *
+     * @param input String.
+     * @return 半角字符串
+     */
+    public static String ToDBC(String input) {
+
+
+        char c[] = input.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] == '\u3000') {
+                c[i] = ' ';
+            } else if (c[i] > '\uFF00' && c[i] < '\uFF5F') {
+                c[i] = (char) (c[i] - 65248);
+
+            }
+        }
+        String returnString = new String(c);
+
+        return returnString;
+    }
 
 }
